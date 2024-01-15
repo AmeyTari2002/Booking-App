78068dd187fe95b2f3e5c93e2f0bfee00ecd76b4
AmeyTari2002 <116430042+AmeyTari2002@users.noreply.github.com>
2024-01-15 16:04:38 +0530

    Update Image.jsx

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,7 +1,8 @@
 export default function Image({ src, ...rest }) {
-    src = src && src.includes('https://') 
+    const baseUrl = import.meta.env.VITE_API_BASE_URL_IMAGES;
+    src = src && src.includes('https://')
     ? src 
-    : 'http://localhost:4000/uploads/' + src;
+    : `${baseUrl}/uploads/${src}`
     return (
         <img{...rest} src={src} alt={''} />
     )
