5bb85350fbff807cb5daad901a4c78a8fb93347a
Amey Prakash Tari <116430042+AmeyTari2002@users.noreply.github.com>
2024-02-14 13:36:00 +0530

    Update PhotosUploader.jsx

--- client/src/pages/PhotosUploader.jsx
+++ client/src/pages/PhotosUploader.jsx
@@ -53,7 +53,7 @@ const PhotosUploader = ({ addedPhotos, onChange }) => {
     return (
         <>
             <div className='flex'>
-                <input value={photoLink} onChange={ev => setphotoLink(ev.target.value)} type="text" placeholder='Add using a Link ......jpg' />
+                <input value={photoLink} required onChange={ev => setphotoLink(ev.target.value)} type="text" placeholder='Add using a Link ......jpg' />
                 <button className='bg-gray-200 px-4  rounded-2xl' onClick={addPhotoByLink}>Add Photo</button>
             </div>
             <div className=' mt-3 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6'>
@@ -82,7 +82,7 @@ const PhotosUploader = ({ addedPhotos, onChange }) => {
                     </div>
                 ))}
                 <label className='h-32 cursor-pointer flex items-center justify-center gap-1  border bg-transparent rounded-2xl p-2 text-2xl text-gray-600'>
-                    <input type="file" multiple className='hidden' onChange={uploadPhoto} />
+                    <input type="file" multiple className='hidden' required onChange={uploadPhoto} />
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokelinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
                     </svg>
