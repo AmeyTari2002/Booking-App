5951ea979a52168aeded812b3a65ebe2b3fc81e9
Amey Prakash Tari <116430042+AmeyTari2002@users.noreply.github.com>
2024-02-14 12:53:02 +0530

    Update Place.js

--- api/models/Place.js
+++ api/models/Place.js
@@ -3,17 +3,17 @@ const mongoose = require('mongoose')
 const {Schema} = mongoose;
 
 const placeSchema = new mongoose.Schema({
-    owner:{type:mongoose.Schema.Types.ObjectId , ref:'User'},
-    title: String,
-    address: String,
-    photos: [String],
+    owner:{type:mongoose.Schema.Types.ObjectId , ref:'User', required: true},
+    title: { type: String, required: true },
+    address: { type: String, required: true },
+    photos: { type: [String], required: true }, // Updated line for the 'photos' field
     description: String,
     perks: [String],
     extraInfo: String,
-    checkIn: Number,
-    checkOut: Number,
-    maxGuest: Number,
-    price: Number
+    checkIn: { type: Number, required: true },
+    checkOut: { type: Number, required: true },
+    maxGuest: { type: Number, required: true },
+    price: { type: Number, required: true }
 })
 
 const PlaceModel = mongoose.model('Place',placeSchema)
