305d40ab846d79c070b7fd263492e9d74ebc5061
Amey Prakash Tari <116430042+AmeyTari2002@users.noreply.github.com>
2024-02-14 12:54:25 +0530

    Update User.js

--- api/models/User.js
+++ api/models/User.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose')
 const {Schema} = mongoose;
 
 const UserSchema = new Schema({
-    name: String,
-    email: {type:String , unique:true},
-    password:String,
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
 })
 
 
